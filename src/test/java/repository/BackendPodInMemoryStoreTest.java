662bb85cb27db223bba2dd1263eb5196edd6e658
Eloy Gustavo Ye Luo <eloiiiig@gmail.com>
2025-02-17 22:23:33 -0800

    Add InMemoryStore Tests
    add in memory store tests for all methods

---
+++ src/test/java/repository/BackendPodInMemoryStoreTest.java
@@ -0,0 +1,160 @@
+package repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pods.BackendPod;
+import utils.EventSubscriber;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class BackendPodInMemoryStoreTest {
+
+    private BackendPodInMemoryStore store;
+
+    @BeforeEach
+    void setUp() {
+        this.store = BackendPodInMemoryStore.getStore();
+    }
+
+    @AfterEach
+    void cleanUp() {
+        BackendPodInMemoryStore.resetInstance();
+    }
+
+    @Test
+    void getStore() {
+        assertNotNull(this.store);
+        assertEquals(BackendPodInMemoryStore.class, this.store.getClass());
+    }
+
+    private BackendPod createBackendPod(String address) throws URISyntaxException {
+        var uri = new URI(address);
+        return new BackendPod(uri, false, 0, false);
+    }
+
+    @Test
+    void getSuccess() {
+        try {
+            String stringAddress = "http://localhost:8080";
+            var backendPod = createBackendPod(stringAddress);
+            this.store.add(backendPod);
+            var resultBackendPod = this.store.get(new URI(stringAddress));
+            assertEquals(resultBackendPod, backendPod);
+        } catch (URISyntaxException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void getFailure() {
+        try {
+            URI id = new URI("http://localhost:8080");
+            assertNull(this.store.get(id));
+        } catch (URISyntaxException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void getAll() {
+        try {
+            var backend1 = createBackendPod("http://localhost:8080");
+            var backend2 = createBackendPod("http://localhost:80");
+            this.store.add(backend1);
+            this.store.add(backend2);
+            var backends = this.store.getAll();
+            assertTrue(backends.containsAll(List.of(backend1, backend2)));
+        } catch (URISyntaxException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void add() {
+        try {
+            var address = "http://localhost:8080";
+            var pod = createBackendPod(address);
+            this.store.add(pod);
+            assertNotNull(this.store.get(new URI(address)));
+        } catch (URISyntaxException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void addInvokesEvent() {
+
+        try {
+            var subscriber = mock(EventSubscriber.class);
+            var address = "http://localhost:8080";
+            var pod = createBackendPod(address);
+            this.store.subscribe(BackendPodEvent.NEW_BACKEND, subscriber);
+            this.store.add(pod);
+            verify(subscriber, times(1)).handleEvent(eq(BackendPodEvent.NEW_BACKEND), any(BackendPodEventContext.class));
+        } catch (URISyntaxException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void remove() {
+        try {
+            String address = "http://localhost:8080";
+            var uri = new URI(address);
+            this.store.add(createBackendPod(address));
+            assertEquals(1, this.store.getAll().size());
+            assertNotNull(this.store.get(uri));
+            this.store.remove(uri);
+            assertEquals(0, this.store.getAll().size());
+            assertNull(this.store.get(uri));
+        } catch (URISyntaxException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void removeInvokesEvent() {
+
+        try {
+            var subscriber = mock(EventSubscriber.class);
+            var address = "http://localhost:8080";
+            this.store.subscribe(BackendPodEvent.REMOVE_BACKEND, subscriber);
+            var pod = createBackendPod(address);
+            this.store.add(pod);
+            this.store.remove(new URI(address));
+            verify(subscriber, times(1)).handleEvent(eq(BackendPodEvent.REMOVE_BACKEND), any(BackendPodEventContext.class));
+        } catch (URISyntaxException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void subscribeAndPublish() {
+        var subscriber = mock(EventSubscriber.class);
+        this.store.subscribe(BackendPodEvent.NEW_BACKEND, subscriber);
+        BackendPodEventContext context = new BackendPodEventContext(BackendPodEvent.NEW_BACKEND, ZonedDateTime.now(), List.of());
+        this.store.publish(BackendPodEvent.NEW_BACKEND, context);
+        verify(subscriber, times(1)).handleEvent(BackendPodEvent.NEW_BACKEND, context);
+    }
+
+    @Test
+    void unsubscribe() {
+        var subscriber = mock(EventSubscriber.class);
+        this.store.subscribe(BackendPodEvent.NEW_BACKEND, subscriber);
+        BackendPodEventContext context = new BackendPodEventContext(BackendPodEvent.NEW_BACKEND, ZonedDateTime.now(), List.of());
+        this.store.publish(BackendPodEvent.NEW_BACKEND, context);
+        verify(subscriber, times(1)).handleEvent(BackendPodEvent.NEW_BACKEND, context);
+        this.store.unsubscribe(BackendPodEvent.NEW_BACKEND, subscriber);
+        BackendPodEventContext newContext = new BackendPodEventContext(BackendPodEvent.NEW_BACKEND, ZonedDateTime.now(), List.of());
+        this.store.publish(BackendPodEvent.NEW_BACKEND, newContext);
+        reset(subscriber);
+        verify(subscriber, times(0)).handleEvent(BackendPodEvent.NEW_BACKEND, newContext);
+    }
+}
